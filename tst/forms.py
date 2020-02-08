3bc6e380c89fc4ccfe11fff47204a12b51a7d415
anoopthiparala <30379425+anoopthiparala@users.noreply.github.com>
2020-02-08 18:21:23 +0530

    update

--- tst/forms.py
+++ tst/forms.py
@@ -4,4 +4,4 @@ from tst.models import Resp
 class RegForm(forms.ModelForm):
     class Meta:
         model = Resp
-        fields = ['roll_no', 'name', 'passkey']
+        fields = ['roll_no', 'name', 'college', 'passkey']
--- tst/models.py
+++ tst/models.py
@@ -3,7 +3,7 @@ from django.db.models import Model
 from django.core.exceptions import ValidationError
 
 def validate_roll(roll_no):
-    if len(roll_no) == 10 and "w91a0" in roll_no.lower():
+    if len(roll_no) == 10:
         return
     else:
         raise ValidationError("INVALID ROLL NO")
@@ -19,10 +19,15 @@ class Resp(Model):
 
     class Meta:
         unique_together = (('roll_no', 'passkey'),)
+
+    CHOICES =( 
+    ("1", "MRIET"), 
+    ("2", "OTHERS") ) 
     
     roll_no = models.CharField(max_length = 10, null = False, blank = False, validators=[validate_roll])
     name = models.CharField(max_length = 50, null = False, blank = False)
     passkey = models.CharField(max_length = 6, null = False, blank = False, validators=[validate_key])
+    college = models.CharField(max_length = 10, null = False, blank = False, choices = CHOICES)
     resp = models.CharField(max_length = 30, null = True, blank = True)
 
 
